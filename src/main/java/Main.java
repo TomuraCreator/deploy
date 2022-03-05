0c1b1e231c8901c1532c46af51878ecd038765eb
rainman-wf <denviva@gmail.com>
2022-03-05 22:30:37 +0300

    Procfile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         XOGame bot = new XOGame();
 
-        Server server = new Server(bot);
-        server.start();
+        bot.listen();
     }
 }
